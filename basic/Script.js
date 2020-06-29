3155228ed7b1e81919012be168ddde4602d541cd
Saikrishnan <krishnansai99@gmail.com>
2020-06-29 22:35:55 +0530

    Javascript Practice Day 1

---
+++ basic/Script.js
@@ -0,0 +1,8 @@
+document.write("Hello Again");
+alert('Hello 3');
+console.log("Hello World");
+console.log(4+6);
+console.error("Error");
+console.warn("Warning");
+console.log("%cHello World","color:green");
+console.log("%cHello World","color:yellow,background:black");
